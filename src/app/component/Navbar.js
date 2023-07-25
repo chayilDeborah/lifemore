f76c1a7983b827c41bb213b6b8a709c8134fbbcf
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-07-25 03:36:13 +0100

    feat: add routing to all navbar links feat: create about page

--- src/app/component/Navbar.js
+++ src/app/component/Navbar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import cyclone from "../assets/cyclone.svg";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -15,11 +16,11 @@ const Navbar = () => {
               </div>
             </div>
             <div className="flex justify-between w-[652px] items-center">
-                <div className="text-[#22366C] text-[20px] leading-[32.4px] hover:bg-violet-600 hover:bg-[#7537F6] cursor-pointer">Home</div>
-                <div className="text-[#22366C] text-[20px] leading-[32.4px]">About Us</div>
-                <div className="text-[#22366C] text-[20px] leading-[32.4px]">Events</div>
-                <div className="text-[#22366C] text-[20px] leading-[32.4px]">Contact</div> 
-                <button className="bg-[#7537F6] text-[#fff] text-[20px] leading-[32.4px] opacity-[0.699999988079071] px-[24px] py-[16px] hover:text-[red]">Join Us</button>
+                <div className="text-[#22366C] text-[20px] leading-[32.4px] hover:bg-[#7537F6] hover:opacity-[0.699999988079071] hover:text-[#fff] cursor-pointer hover:px-[12px] hover:py-[8px]"><Link href="/">Home</Link></div>
+                <div className="text-[#22366C] text-[20px] leading-[32.4px] hover:bg-[#7537F6] hover:opacity-[0.699999988079071] hover:text-[#fff] cursor-pointer hover:px-[12px] hover:py-[8px]"><Link href="/about">About Us</Link></div>
+                <div className="text-[#22366C] text-[20px] leading-[32.4px] hover:bg-[#7537F6] hover:opacity-[0.699999988079071] hover:text-[#fff] cursor-pointer hover:px-[12px] hover:py-[8px]"><Link href="/events">Events</Link></div>
+                <div className="text-[#22366C] text-[20px] leading-[32.4px] hover:bg-[#7537F6] hover:opacity-[0.699999988079071] hover:text-[#fff] cursor-pointer hover:px-[12px] hover:py-[8px]"><Link href="/contact">Contact</Link></div> 
+                <button className="bg-[#7537F6] text-[#fff] text-[20px] leading-[32.4px] opacity-[0.699999988079071] px-[24px] py-[16px] hover:text-[white] hover:bg-[#7537F6] hover:opacity-[1]">Join Us</button>
 
             </div>
           </div>
