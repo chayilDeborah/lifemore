90194a951105c8f9a41360bf15b3489c8ffe1c17
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-07-29 09:43:44 +0100

    feat: fix mobile responsiveness

--- src/app/component/AboutPreview.jsx
+++ src/app/component/AboutPreview.jsx
@@ -7,16 +7,16 @@ import Link from "next/link";
 const AboutPreview = () => {
   return (
     <>
-      <div className="flex">
-        <Image src={aboutimg} alt="icon" className="mt-[100px] mr-[73px]" />
+      <div className="flex w-[90%] lg:w-[100%] mx-auto lg:mx-[0] flex-col-reverse lg:flex-row lg:flex">
+        <Image src={aboutimg} alt="icon" className="lg:mt-[100px] mt-[60px] lg:mr-[73px]" />
         <div className="">
-          <div className="text-[#716B95] text-[12px] leading-[19.5px] tracking-[1.3px] mt-[157px] mb-[15.69px] ">
+          <div className="text-[#716B95] text-[12px] leading-[19.5px] tracking-[1.3px] lg:mt-[157px] mt-[130px] text-center lg:text-left mb-[15.69px] ">
             ABOUT LIFEMORE COMMUNITY
           </div>
-          <div className="text-[#22366C] text-[45px] leading-[52.8px] mb-[17.9px]">
+          <div className="text-[#22366C] text-[35px] text-center lg:text-left lg:text-[45px] leading-[48.8px] lg:leading-[52.8px] mb-[15.9px] lg:mb-[17.9px]">
             Connecting Minds, Empowering Growth.
           </div>
-          <div className="text-[#22366C] text-[17px] leading-[32.4px] w-[492px] mb-[28.58px]">
+          <div className="text-[#22366C] text-[17px] leading-[32.4px] lg:w-[492px] mb-[15.28px] lg:mb-[28.58px]">
             We believe in the power of connections. We understand that growth
             and success are not achieved in isolation, but through collaboration
             and support from a network of like-minded individuals.
