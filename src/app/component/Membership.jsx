4430ba104da4bd333ad7edb1cf0f20050e80b4a4
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-08-02 20:07:41 +0100

    feat: embed google form for membership

--- src/app/component/Membership.jsx
+++ src/app/component/Membership.jsx
@@ -1,50 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import color from "../assets/color.svg";
 
 const Membership = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Sending");
-
-    let data = {
-      firstName,
-      lastName,
-      phone,
-      email,
-    };
-
-    fetch("/api/member", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => {
-        console.log("Response received");
-        if (res.status === 200) {
-          console.log("Response succeeded!");
-          setSubmitted(true);
-          setFirstName("");
-          setLastName("");
-          setEmail("");
-          setPhone("");
-        }
-      })
-      .catch((error) => {
-        console.error("Error occurred during form submission:", error);
-      });
-      console.log('Button clicked')
-  };
 
   return (
     <>
@@ -57,102 +16,24 @@ const Membership = () => {
                 alt="icon"
                 className="ml-auto mt-[20px] mr-[23px] lg:mt-[27px] lg:mb-[17px] mb-[10px] lg:w-[42px] lg:h-[41px] w-[32px] h-[31px]"
               />
-              <div className="w-[100%] max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 ">
-                <div className="lg:mr-[80px] w-[90%] mx-auto lg:mx-[0px] lg:w-[100%]">
-                  <div className="text-[#413972] text-[12px] leading-[19.5px] tracking-[1.3px] mb-[8px] lg:mb-[16px]  mt-[10px] lg:mt-[37px]">
+              <div className="w-[100%] max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-1 ">
+                <div>
+                <div className="text-[#413972] text-[12px] leading-[19.5px] tracking-[1.3px] mb-[8px] lg:mb-[16px]  mt-[10px] lg:mt-[7px]">
                     REGISTER
                   </div>
-                  <div className="text-[36px] lg:text-[46px] leading-[42.8px] lg:leading-[52.8px] text-[#12084F] w-[290px] lg:w-[350px] mb-[8px] lg:mb-[16px]">
-                    Become a Member Today.
+                  <div className="mt-[30px] lg:mt-[0px] lg:-ml-[54px]">
+                    <iframe
+                      src="https://docs.google.com/forms/d/e/1FAIpQLSejEJm5Y0MmzplwSbL0y_0fRMCCiQZg1woh0yj57Wck-3QvoA/viewform?embedded=true"
+                      className="lg:w-[750px] w-[300px] h-[1587px] lg:h-[1450px]"
+                      // width="750"
+                      // height="1466"
+                      frameborder="0"
+                      marginheight="0"
+                      marginwidth="0"
+                    >
+                      Loading…
+                    </iframe>
                   </div>
-                  <div className="text-[16px] leading-[28.4px] lg:leading-[32.4px] text-[#413972] font-bold lg:w-[327px] mb-[12px] lg:mb-[26px]">
-                    Register to become a member of Lifemore Global Community!
-                  </div>
-                  <div className="text-[#413972] text-[17px] leading-[28.4px] lg:leading-[32.4px] lg:w-[398px] ">
-                    As a member, you’ll gain access to valuable resources,
-                    networking opportunities, and a supportive community that
-                    will help you achieve your goals faster and more
-                    efficiently.
-                  </div>
-                </div>
-                <div>
-                  <form
-                  
-                    className="mt-[30px] lg:mt-[0px]"
-                  >
-                    <div className="grid gap-6 mb-6 lg:grid-cols-2 w-[85%] mx-auto lg:mx-[0px] lg:w-[100%]">
-                      <div>
-                        <label
-                          for="first_name"
-                          class="block mb-[9.19px] text-[13px] text-[#413972] leading-[14px]"
-                        >
-                          First name *
-                        </label>
-                        <input
-                          type="text"
-                          name="firstName"
-                          onChange={(e)=>{setFirstName(e.target.value)}}
-                          className="bg-[#fff] border border-[#A09CB9]  rounded-[4px] focus:ring-[#413972] pl-[10px] focus:border-[#A09CB9] block w-[273px] lg:w-[200px] h-[46px]"
-                          required
-                        />
-                      </div>
-                      <div>
-                        <label
-                          for="last_name"
-                          className="block mb-[9.19px] text-[13px] text-[#413972] leading-[14px] "
-                        >
-                          Last name *
-                        </label>
-                        <input
-                          type="text"
-                          name="lastName"
-                          onChange={(e)=>{setLastName(e.target.value)}}
-                          className="bg-[#fff] border border-[#A09CB9]  rounded-[4px] focus:ring-[#413972] pl-[10px] focus:border-[#A09CB9] block w-[273px] lg:w-[200px] h-[46px]"
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="w-[85%] mx-auto lg:mx-[0px] lg:w-[100%]">
-                      <div className="mb-6">
-                        <label
-                          for="phone"
-                          class="block mb-[9.19px] text-[13px] text-[#413972] leading-[14px] "
-                        >
-                          Phone number *
-                        </label>
-                        <input
-                          type="tel"
-                          name="phone"
-                          onChange={(e)=>{setPhone(e.target.value)}}
-                          className="bg-[#fff] border border-[#A09CB9]  rounded-[4px] focus:ring-[#413972] focus:border-[#A09CB9] block pl-[10px] w-[273px] lg:w-[452px] h-[46px]"
-                          // pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                          required
-                        />
-                      </div>
-                      <div className="mb-6">
-                        <label
-                          for="email"
-                          className="block mb-[9.19px] text-[13px] text-[#413972] leading-[14px] "
-                        >
-                          Email address *
-                        </label>
-                        <input
-                          type="email"
-                          name="email"
-                          onChange={(e)=>{setEmail(e.target.value)}}
-                          className="bg-[#fff] border border-[#A09CB9]  rounded-[4px] focus:ring-[#413972] focus:border-[#A09CB9] block pl-[10px] w-[273px] lg:w-[452px] h-[46px]"
-                          required
-                        />
-                      </div>
-                      <button
-                        type="submit"
-                        onClick={(e)=>{handleSubmit(e)}}
-                        className="bg-[#7537F6] text-[#fff] text-[14.56px] font-black leading-[18.216px] tracking-[1.159px] w-[273px] lg:w-[452px] hover:opacity-[0.699999988079071] rounded-[6px] mb-[100px] lg:mb-[200px] py-[15px]"
-                      >
-                        Sign up for Membership
-                      </button>
-                    </div>
-                  </form>
                 </div>
               </div>
             </div>
