6b098e88c082fa84f301b166c24b7f7115e11496
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-08-06 13:53:56 +0100

    style: increase team grid spacing

--- src/app/component/Team.jsx
+++ src/app/component/Team.jsx
@@ -14,18 +14,18 @@ const Team = () => {
           At Lifemore Global Community, we’re proud to have a talented and
           experience team that is dedicated to supporting growth.
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 mb-[100px]">
+        <div className="grid grid-cols-1 space-y-[25px] lg:space-y-[0px] lg:grid-cols-3 mb-[100px]">
           <div>
-            <Image src={inspiration} alt="icon" className="w-[338px] h-[385px] lg:w-[358px] lg:h-[405px] mx-auto lg:mx-[0]"/>
+            <Image src={inspiration} alt="icon" className="w-[298px] h-[345px] lg:w-[358px] lg:h-[405px] mx-auto lg:mx-[0]"/>
             <div className="text-[#000] text-[20px] leading-[150%] mb-[10px] lg:mt-[24px] mt-[15px]  font-semibold text-center">
-              Ayodeji Inspiration{" "}
+              Amb. Ayodeji Inspiration{" "}
             </div>
             <div className="text-[#000]  text-[12px] leading-[19.5px] tracking-[1.3px] lg:mb-[0px] mb-[20px] leading-[150%] text-center">
               LEAD
             </div>
           </div>
           <div>
-            <Image src={ayo} alt="icon" className="w-[338px] h-[385px] lg:w-[358px] lg:h-[405px] mx-auto lg:mx-[0]"/>
+            <Image src={ayo} alt="icon" className="w-[298px] h-[345px] lg:w-[358px] lg:h-[405px] mx-auto lg:mx-[0]"/>
             <div className="text-[#000] text-[20px] leading-[150%] mb-[10px] lg:mt-[24px] mt-[15px] font-semibold text-center">
               Adenjolaoluwa Ayodeji{" "}
             </div>
@@ -34,7 +34,7 @@ const Team = () => {
             </div>
           </div>
           <div>
-            <Image src={funmi} alt="icon" className="w-[338px] h-[385px] lg:w-[358px] lg:h-[405px] mx-auto lg:mx-[0]"/>
+            <Image src={funmi} alt="icon" className="w-[298px] h-[345px] lg:w-[358px] lg:h-[405px] mx-auto lg:mx-[0]"/>
             <div className="text-[#000] text-[20px] leading-[150%] mb-[10px] lg:mt-[24px] mt-[15px] font-semibold text-center">
             Olonisakin Funmilayo
             </div>
