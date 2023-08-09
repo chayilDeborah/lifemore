f9898a5b0402631e1d25bc26a5c2027f701a5e27
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-08-09 17:02:32 +0100

    fix: responsiveness fix

--- src/app/component/EventLists.jsx
+++ src/app/component/EventLists.jsx
@@ -12,7 +12,7 @@ const EventLists = () => {
       <div className="flex justify-center">
         <div className="max-w-7xl w-[100%]">
           <div id="year2022">
-            <div className="text-[#22366C] text-[30px] lg:text-[48px] font-bold leading-[120%] mt-[60px] lg:mt-[112px] mb-[24px] lg:ml-[20%] text-center lg:text-left">
+            <div className="text-[#22366C] text-[30px] lg:text-[48px] font-bold leading-[120%] mt-[60px] lg:mt-[112px] mb-[24px] lg:ml-[60px] text-center lg:text-left">
               Year 2022
             </div>
             <div className="grid lg:grid-cols-2 grid-cols-1 mx-auto lg:mx-[0px] space-y-[45px] lg:space-y-[0px]">
@@ -34,7 +34,7 @@ const EventLists = () => {
             </div>
           </div>
           <div id="year2023">
-            <div className="text-[#22366C] text-[30px] lg:text-[48px] font-bold leading-[120%] lg:mt-[112px] mt-[50px] mb-[24px] text-center lg:text-left lg:ml-[20%]">
+            <div className="text-[#22366C] text-[30px] lg:text-[48px] font-bold leading-[120%] lg:mt-[112px] mt-[50px] mb-[24px] text-center lg:text-left lg:ml-[60px]">
               Year 2023
             </div>
             <div className="grid lg:grid-cols-2 grid-cols-1">
--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -13,7 +13,7 @@ const Events = () => {
         <div className=" w-[100%]">
           <div className="h-screen bg-[url('https://res.cloudinary.com/dmye53wps/image/upload/v1690471382/events_rahcty.jpg')] bg-cover bg-center w-full">
             <div className="w-full h-full flex items-end bg-blue-600/30 bg-gradient-to-r from-[#22366C] to-[] lightgray 50% backdrop-brightness-75 bg-gradient-to-l">
-              <div className="flex max-w-7xl">
+              <div className="flex max-w-7xl mx-auto w-[100%]">
               <div className="pt-[25%]  w-[90%] mx-auto">
                 <div className="lg:text-[56px] text-[50px] font-bold leading-[100%] lg:leading-[120%] text-[white] mb-[12px]">Our Events</div>
                 <div className="text-[white] text-[18px] w-[90%] lg:w-[462px] mb-[24px]">Connect with like-minded individuals, share ideas, and unlock your potential through dynamic discussions and engaging events. </div>
