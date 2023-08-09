b47b8a2bee779428f4751c8d2baa92ec098413e6
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-08-09 16:43:07 +0100

    feat: update event content

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -13,14 +13,16 @@ const Events = () => {
         <div className=" w-[100%]">
           <div className="h-screen bg-[url('https://res.cloudinary.com/dmye53wps/image/upload/v1690471382/events_rahcty.jpg')] bg-cover bg-center w-full">
             <div className="w-full h-full flex items-end bg-blue-600/30 bg-gradient-to-r from-[#22366C] to-[] lightgray 50% backdrop-brightness-75 bg-gradient-to-l">
-              <div className="pt-[25%] lg:w-full w-[90%] mx-auto lg:mx-[0px]">
-                <div className="lg:text-[56px] text-[50px] font-bold leading-[100%] lg:leading-[120%] text-[white] mb-[15px] lg:mb-[24px]">Our Events</div>
-                <div className="text-[white] text-[18px] lg:w-[462px] mb-[24px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </div>
+              <div className="flex max-w-7xl">
+              <div className="pt-[25%]  w-[90%] mx-auto">
+                <div className="lg:text-[56px] text-[50px] font-bold leading-[100%] lg:leading-[120%] text-[white] mb-[12px]">Our Events</div>
+                <div className="text-[white] text-[18px] w-[90%] lg:w-[462px] mb-[24px]">Connect with like-minded individuals, share ideas, and unlock your potential through dynamic discussions and engaging events. </div>
                 <div className="grid grid-cols-2 space-x-[px] w-[150px] mb-[35px] ">
                   {/* <button className="bg-[#F4F4F4] text-[14px] leading-[150%] w-[48px] text-[#000] ">2018</button> */}
                   <button className="bg-[#F4F4F4] text-[14px] leading-[150%] w-[48px] text-[#000] " ><Link href="#year2022">2022</Link></button>
                   <button className="bg-[#F4F4F4] text-[14px] leading-[150%] w-[48px] text-[#000]"><Link href="#year2023">2023</Link></button>
                 </div>
+              </div>
               </div>
             </div>
           </div>
